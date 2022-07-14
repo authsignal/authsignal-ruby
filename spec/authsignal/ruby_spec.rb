f0b498fe50434bf861218a9b5da36f2385ece8b9
justinsoong <justinsoong@gmail.com>
2022-07-14 15:17:58 +1200

    tests

--- spec/authsignal/ruby_spec.rb
+++ spec/authsignal/ruby_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe Authsignal::Ruby do
   it "has a version number" do
-    expect(Authsignal::Ruby::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(Authsignal::VERSION).not_to be nil
   end
 end
